dcfc61bcfdb67d268e0124cdae3353541f574a99
Paskal Nicolas <paskal.nicolas@hotmail.com>
2026-06-15 02:12:24 +0200

    feat: add vectorstore creation with HuggingFace embeddings and ChromaDB persistence

--- src/ingest.py
+++ src/ingest.py
@@ -4,6 +4,8 @@ import yaml
 from loguru import logger
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 import click
 
 def load_config(config_path:str) -> dict:
@@ -36,7 +38,7 @@ def load_documents_from_directory(pdf_directory: str) -> list:
 
 def chunk_documents(documents:list, chunk_config:dict) -> list:
     if not documents:
-        logger.error("No document to chunk")
+        logger.error("No documents to chunk")
         raise ValueError("No documents to chunk")
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = chunk_config["chunk_size"],
@@ -51,12 +53,31 @@ def chunk_documents(documents:list, chunk_config:dict) -> list:
     return chunks_documents
 
 
+def build_vectorstore(chunks:list, vectorstore_config:dict):
+    if not chunks:
+        logger.error("No chunks to embed")
+        raise ValueError("No chunks to embed")
+    
+    hf = HuggingFaceEmbeddings(
+        model_name=vectorstore_config["embedding_model"]
+    )
+   
+    vectorstore = Chroma.from_documents (
+        documents = chunks,
+        embedding = hf,
+        persist_directory = vectorstore_config["persist_dir"])
+    logger.info(f"Vectorstore created at {vectorstore_config['persist_dir']}")
+
+
+
 @click.command()
 @click.option('--config','-c','config')
 def main(config):
         config_loaded=load_config(config)
         pdfs_loaded=load_documents_from_directory(config_loaded["data"]["source_dir"])
-        chunk_documents(pdfs_loaded,config_loaded["chunking"])
+        documents_chunked=chunk_documents(pdfs_loaded,config_loaded["chunking"])
+        build_vectorstore(documents_chunked,config_loaded["vectorstore"])
+
        
 
 if __name__ == "__main__":
